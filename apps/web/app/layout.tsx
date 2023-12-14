38170deebf4876ccae4c0920cdefae45339605db
dontfred <mail@frederikwgrimm.de>
2023-12-14 16:52:35 +0100

    fix: :ambulance: Fixed Dark mode

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html className="bg-white dark:bg-black" lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body className={`${inter.className} dark`}>{children}</body>
     </html>
   );
 }
